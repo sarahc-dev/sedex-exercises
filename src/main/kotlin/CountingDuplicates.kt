0e381ec97129849ff603a8428bb117a7cdea9438
Sarah Clements <sarahclements@hotmail.co.uk>
2024-05-29 14:38:32 +0100

    add further tests from codewars

--- src/main/kotlin/CountingDuplicates.kt
+++ src/main/kotlin/CountingDuplicates.kt
@@ -2,7 +2,6 @@ package org.example
 
 class CountingDuplicates {
     fun duplicateCount(text: String): Int {
-        // any char that is duplicated
         val groupChars = text.groupingBy { it.lowercase() }.eachCount()
         return groupChars.count { it.value > 1 }
     }
--- src/test/kotlin/org/example/CountingDuplicatesTest.kt
+++ src/test/kotlin/org/example/CountingDuplicatesTest.kt
@@ -35,4 +35,25 @@ class CountingDuplicatesTest {
         val actual = CountingDuplicates().duplicateCount("aAbb")
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun `If input is abcde, return 0`() {
+        val expected = 0
+        val actual = CountingDuplicates().duplicateCount("abcde")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `If input is aabBcde, return 2`() {
+        val expected = 2
+        val actual = CountingDuplicates().duplicateCount("aabBcde")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `If input is Indivisibilities, return 2`() {
+        val expected = 2
+        val actual = CountingDuplicates().duplicateCount("Indivisibilities")
+        assertEquals(expected, actual)
+    }
 }
