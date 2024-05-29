645bba3dd6ce15c1ffbecf8502153d83340d6919
Sarah Clements <sarahclements@hotmail.co.uk>
2024-05-29 14:35:07 +0100

    test passing

--- src/main/kotlin/CountingDuplicates.kt
+++ src/main/kotlin/CountingDuplicates.kt
@@ -3,7 +3,7 @@ package org.example
 class CountingDuplicates {
     fun duplicateCount(text: String): Int {
         // any char that is duplicated
-        val groupChars = text.groupingBy { it }.eachCount()
+        val groupChars = text.groupingBy { it.lowercase() }.eachCount()
         return groupChars.count { it.value > 1 }
     }
 }
