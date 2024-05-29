e3667971058061219096d28b350ebac5f38c6a3c
Sarah Clements <sarahclements@hotmail.co.uk>
2024-05-29 14:11:28 +0100

    add failing test

--- src/main/kotlin/CountingDuplicates.kt
+++ src/main/kotlin/CountingDuplicates.kt
@@ -2,6 +2,7 @@ package org.example
 
 class CountingDuplicates {
     fun duplicateCount(text: String): Int {
+        // any char that is duplicated
         return 0
     }
 }
--- src/test/kotlin/org/example/CountingDuplicatesTest.kt
+++ src/test/kotlin/org/example/CountingDuplicatesTest.kt
@@ -5,13 +5,20 @@ import org.junit.jupiter.api.Test
 
 class CountingDuplicatesTest {
     @Test
-    fun `If input is a, return (`() {
+    fun `If input is a, return 0`() {
         // Arrange
         val countingDuplicates = CountingDuplicates()
         val expected = 0
         // Act
         val actual = countingDuplicates.duplicateCount("a")
         // Assert
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `If input is aa, return 1`() {
+        val expected = 1
+        val actual = CountingDuplicates().duplicateCount("aa")
         assertEquals(expected, actual)
     }
 }
